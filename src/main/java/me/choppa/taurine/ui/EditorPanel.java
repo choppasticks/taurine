3dabc55c56a41991ff869f112f72be256d247f7a
choppa <brandonallan6@gmail.com>
2026-04-20 20:07:19 -0400

    new

--- src/main/java/me/choppa/taurine/ui/EditorPanel.java
+++ src/main/java/me/choppa/taurine/ui/EditorPanel.java
@@ -11,79 +11,80 @@ import me.choppa.taurine.editor.BufferHandler;
 
 public class EditorPanel extends JPanel {
 
-  private Font font = new Font("Monospaced", Font.PLAIN, 16);
-  private Color backgroundColor = new Color(75, 75, 150);
-  private Color foregroundColor = new Color(Color.WHITE.getRGB());
+    private Font font = new Font("Monospaced", Font.PLAIN, 16);
+    private Color backgroundColor = new Color(75, 75, 150);
+    private Color foregroundColor = new Color(Color.WHITE.getRGB());
 
-  private final Taurine taurine = Taurine.getInstance();
+    private final Taurine taurine = Taurine.getInstance();
 
-  public EditorPanel() {
-    this.setLayout(new BorderLayout());
-    this.setFocusable(true);
-  }
-
-  @Override
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    g.setFont(font);
-
-    FontMetrics fontMetrics = g.getFontMetrics();
-
-    TabHandler tabHandler = taurine.getTabHandler();
-
-    BufferHandler buffer = tabHandler.getActiveTab();
-
-    /*
-     * int tabGap = 0;
-     * for (BufferHandler bufferHandler : tabHandler.getTabs()) {
-     *
-     * int x = tabGap += 30;
-     * int y = 2;
-     * int width = 30;v
-     * int height = 20;
-     * if (bufferHandler == buffer) {
-     * g.setColor(Color.GREEN);
-     * } else {
-     * g.setColor(Color.WHITE);
-     * }
-     * g.fillRect(x, y, width, height);
-     * g.setColor(Color.BLACK);
-     * g.drawString(bufferHandler.getName(), x + 5, y + 15);
-     *
-     * }
-     */
-
-    int lineHeight = fontMetrics.getHeight();
-
-    setBackground(backgroundColor);
-    setForeground(foregroundColor);
-
-    int distanceFromTop = 40;
-    int textY = distanceFromTop;
-
-    int lineCount = 1;
-
-    int gutterWidth = 30;
-    for (StringBuilder stringBuilder : buffer.getLines()) {
-      g.drawString(stringBuilder.toString(), gutterWidth, textY);
-      String lineNumb = String.format("%2d", lineCount);
-      g.drawString(lineNumb, 2, textY);
-      lineCount++;
-      textY += lineHeight;
+    public EditorPanel() {
+        this.setLayout(new BorderLayout());
+        this.setFocusable(true);
     }
 
-    int cursorRow = buffer.getCursorRow();
-    int cursorColumn = buffer.getCursorColumn();
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(font);
 
-    String currentLine = buffer.getLines().get(cursorRow).toString();
-    String beforeCursor = currentLine.substring(
-        0,
-        Math.min(cursorColumn, currentLine.length()));
+        FontMetrics fontMetrics = g.getFontMetrics();
 
-    int cursorX = gutterWidth + fontMetrics.stringWidth(beforeCursor);
-    int cursorYTop = distanceFromTop + cursorRow * lineHeight;
-    int cursorYBottom = cursorYTop + lineHeight;
+        TabHandler tabHandler = taurine.getTabHandler();
 
-    g.drawLine(cursorX, cursorYTop, cursorX, cursorYBottom);
-  }
+        BufferHandler buffer = tabHandler.getActiveTab();
+
+        /*
+         * int tabGap = 0;
+         * for (BufferHandler bufferHandler : tabHandler.getTabs()) {
+         *
+         * int x = tabGap += 30;
+         * int y = 2;
+         * int width = 30;v
+         * int height = 20;
+         * if (bufferHandler == buffer) {
+         * g.setColor(Color.GREEN);
+         * } else {
+         * g.setColor(Color.WHITE);
+         * }
+         * g.fillRect(x, y, width, height);
+         * g.setColor(Color.BLACK);
+         * g.drawString(bufferHandler.getName(), x + 5, y + 15);
+         *
+         * }
+         */
+
+        int lineHeight = fontMetrics.getHeight();
+
+        setBackground(backgroundColor);
+        setForeground(foregroundColor);
+
+        int distanceFromTop = 40;
+        int textY = distanceFromTop;
+
+        int lineCount = 1;
+
+        int gutterWidth = 30;
+        for (StringBuilder stringBuilder : buffer.getLines()) {
+            g.drawString(stringBuilder.toString(), gutterWidth, textY);
+            String lineNumb = String.format("%2d", lineCount);
+            g.drawString(lineNumb, 2, textY);
+            lineCount++;
+            textY += lineHeight;
+        }
+
+        int cursorRow = buffer.getCursorRow();
+        int cursorColumn = buffer.getCursorColumn();
+
+        String currentLine = buffer.getLines().get(cursorRow).toString();
+        String beforeCursor = currentLine.substring(
+            0,
+            Math.min(cursorColumn, currentLine.length())
+        );
+
+        int cursorX = gutterWidth + fontMetrics.stringWidth(beforeCursor);
+        int cursorYTop = distanceFromTop + cursorRow * lineHeight;
+        int cursorYBottom = cursorYTop + lineHeight;
+
+        g.drawLine(cursorX, cursorYTop, cursorX, cursorYBottom);
+    }
 }
